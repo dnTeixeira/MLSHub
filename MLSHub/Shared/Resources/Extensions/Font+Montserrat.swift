3b8e7c1e64e1476095a08684072cfd7fca2d09f0
Daniel Teixeira <danielteixeira33@hotmail.com>
2025-07-01 17:27:03 -0300

    style: add helper extensions for custom fonts and colors

---
+++ MLSHub/Shared/Resources/Extensions/Font+Montserrat.swift
@@ -0,0 +1,16 @@
+import Foundation
+import SwiftUICore
+
+extension Font {
+    static func montserratBold(size: CGFloat) -> Font {
+        Font.custom("Montserrat-Bold", size: size)
+    }
+    
+    static func montserratMedium(size: CGFloat) -> Font {
+        Font.custom("Montserrat-Medium", size: size)
+    }
+    
+    static func montserratLight(size: CGFloat) -> Font {
+        Font.custom("Montserrat-Light", size: size)
+    }
+}
